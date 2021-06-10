264b7104e92e07e7ab896c13a56eaf8e70cc68d9
Josh015 <joshua.d.ols@gmail.com>
2021-06-10 07:54:35 -0400

    Added cool SimpleChanges alternative.

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './utils/key';
+export * from './utils/ng-changes';
 export * from './validators/y2k.validator';
 export * from './validators/year2012.validator';
 export * from './shared.module';
---
+++ src/app/shared/utils/ng-changes.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+// Added Partial<> since all change fields are potentially undefined!
+// https://netbasal.com/create-a-typed-version-of-simplechanges-in-angular-451f86593003
+type MarkFunctionProperties<Component> = {
+  [Key in keyof Component]: Component[Key] extends Function ? never : Key;
+};
+type ExcludeFunctionPropertyNames<T> = MarkFunctionProperties<T>[keyof T];
+type ExcludeFunctions<T> = Pick<T, ExcludeFunctionPropertyNames<T>>;
+export type NgChanges<Component, Props = ExcludeFunctions<Component>> = Partial<
+  {
+    [Key in keyof Props]: {
+      previousValue: Props[Key];
+      currentValue: Props[Key];
+      firstChange: boolean;
+      isFirstChange(): boolean;
+    };
+  }
+>;
+/* eslint-enable */
