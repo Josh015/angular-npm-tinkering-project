a348bacd3ebed3b6a1c7cb696d167e235ce4556c
Josh015 <joshua.d.ols@gmail.com>
2022-02-16 22:19:07 -0500

    Removed depreceated code

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,4 +1,3 @@
-export * from './types/controls-group';
 export * from './types/ng-changes';
 export * from './validators/y2k.validator';
 export * from './validators/year2012.validator';
--- src/app/utils/types/controls-group.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from '@angular/forms';
-
-/**
- * When casting a `FormGroup` config object with this type it will constrain
- * the object's field names to the fields present in `T` and ensure that they
- * are affected by refactor commands.
- */
-export type ControlsGroup<T> = { [k in keyof T]: FormControl };
