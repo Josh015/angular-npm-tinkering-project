12262f1fe04639df2e7163ae57fa3108635c5caa
josh015 <joshua.d.ols@gmail.com>
2024-05-24 10:01:45 -0400

    Switched to local JSON user dummy data.

--- src/app/contact-manager/contact-manager.service.ts
+++ src/app/contact-manager/contact-manager.service.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { delay, finalize, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, finalize, tap } from 'rxjs/operators';
 
-import { AVATARS, GENDERS, User } from './models';
+import { User } from './models';
 
 @Injectable()
 export class ContactManagerService {
-  static readonly rootUrl = 'https://dummyapi.online/api';
-  static readonly usersUrl = `${ContactManagerService.rootUrl}/users` as const;
+  static readonly dbDelayMilliseconds = 600;
 
   private readonly http = inject(HttpClient);
 
@@ -19,70 +18,39 @@ export class ContactManagerService {
   private readonly usersSubject$ = new BehaviorSubject<User[]>([]);
   readonly users$ = this.usersSubject$.asObservable();
 
-  private ids: number[] = [];
+  private dataStore: { users: User[] } = { users: [] };
 
   addUser(user: User): Observable<User> {
-    // Needed to simulate saving users to DB.
-    const id: number = Math.max.apply(this, this.ids) + 1;
-    this.ids.push(id);
-    return of({
-      ...user,
-      id,
-    }).pipe(delay(600)); // Delay to show loading spinner
+    user.id = this.dataStore.users.length + 1;
+    this.dataStore.users.push(user);
+    this.usersSubject$.next(this.dataStore.users);
+
+    return of(user).pipe(delay(ContactManagerService.dbDelayMilliseconds)); // Delay to show loading spinner
   }
 
+  // userById(id: number) {
+  //   return this.dataStore.users.find((x) => x.id == id);
+  // }
+
   fetchUsers(): Observable<User[]> {
+    const usersUrl = '/assets/users.json';
+
     this.usersLoadingSubject$.next(true);
 
-    return this.http.get<User[]>(ContactManagerService.usersUrl).pipe(
+    return this.http.get<User[]>(usersUrl).pipe(
       // Delay to show loading spinner
-      delay(600),
-
-      // Keep a list of used IDs for adding new users.
+      delay(ContactManagerService.dbDelayMilliseconds),
       tap((users) => {
-        this.ids = users.map((user) => user.id ?? 0).filter((id) => id);
+        this.dataStore.users = users;
+        this.usersSubject$.next(users);
       }),
-
-      // Add missing fields for dummy data.
-      tap((users) => {
-        for (const [index, user] of users.entries()) {
-          user.birthDate = new Date();
-          user.birthDate.setDate(user.birthDate.getDate() + index);
-
-          user.bio = (user as any).email;
-          user.avatar = AVATARS[index % AVATARS.length];
-          user.gender = GENDERS[index % GENDERS.length];
-
-          user.notes = Object.values(
-            (user as any).address as Map<string, string>,
-          ).map((value, index) => {
-            return {
-              id: index + 1,
-              date: new Date(),
-              title: value,
-            };
-          });
-        }
+      catchError((error: unknown) => {
+        console.log('Failed to fetch users');
+        this.dataStore.users = [];
+        this.usersSubject$.next([]);
+        return throwError(() => error);
       }),
-      tap((users) => this.usersSubject$.next(users)),
-      // catchError(this.handleError),
       finalize(() => this.usersLoadingSubject$.next(false)),
     );
   }
-
-  // private handleError(err: unknown): Observable<never> {
-  //   // in a real world app, we may send the server to some remote logging infrastructure
-  //   // instead of just logging it to the console
-  //   let errorMessage: string;
-  //   if (err.error instanceof ErrorEvent) {
-  //     // A client-side or network error occurred. Handle it accordingly.
-  //     errorMessage = `An error occurred: ${err.error.message}`;
-  //   } else {
-  //     // The backend returned an unsuccessful response code.
-  //     // The response body may contain clues as to what went wrong,
-  //     errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
-  //   }
-  //   console.error(err);
-  //   return throwError(errorMessage);
-  // }
 }
