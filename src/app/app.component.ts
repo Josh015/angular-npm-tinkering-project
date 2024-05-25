83902f73d5912b34ef6e9129bc01bb834adc89b9
josh015 <joshua.d.ols@gmail.com>
2024-05-24 21:13:09 -0400

    Cleaned up junk int he AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,6 @@ import { TranslateService } from '@ngx-translate/core';
   imports: [RouterOutlet],
 })
 export class AppComponent {
-  readonly title = 'angular-npm-tinkering-project';
-
   constructor(translate: TranslateService) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
