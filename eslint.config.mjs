1068eb1a0aab46f58f7964e17a92baad22d4d1be
josh015 <joshua.d.ols@gmail.com>
2024-06-03 18:09:19 -0400

    Switched back to karma+jasmine

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,6 @@ export default ts.config(
       globals: {
         ...globals.browser,
         ...globals.jasmine,
-        ...globals.jest,
         ...globals.jquery,
         ...globals.node,
       },
